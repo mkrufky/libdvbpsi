cd51369a4ac813dd54059974318e045de0def590
Roberto Corno <corno.roberto@gmail.com>
2012-03-25 20:46:30 +0200

    Add descriptor 0x40
    Signed-off-by: Jean-Paul Saman <jpsaman@videolan.org>

--- src/descriptors/dr.h
+++ src/descriptors/dr.h
@@ -46,6 +46,7 @@
 #include "dr_0d.h"
 #include "dr_0e.h"
 #include "dr_0f.h"
+#include "dr_40.h"
 #include "dr_42.h"
 #include "dr_43.h"
 #include "dr_44.h"
---
+++ src/descriptors/dr_40.c
@@ -0,0 +1,109 @@
+/*
+ * dr_40.c
+ * Copyright (C) 2001-2011 VideoLAN
+ *
+ * Authors: rcorno (Nov 22, 2011)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#if defined(HAVE_INTTYPES_H)
+#include <inttypes.h>
+#elif defined(HAVE_STDINT_H)
+#include <stdint.h>
+#endif
+
+#include "../dvbpsi.h"
+#include "../dvbpsi_private.h"
+#include "../descriptor.h"
+
+#include "dr_40.h"
+
+/*****************************************************************************
+ * dvbpsi_DecodeNetworkNameDr
+ *****************************************************************************/
+dvbpsi_network_name_dr_t* dvbpsi_DecodeNetworkNameDr(
+                                        dvbpsi_descriptor_t * p_descriptor)
+{
+    dvbpsi_network_name_dr_t * p_decoded;
+
+    /* Check the tag */
+    if (p_descriptor->i_tag != 0x40)
+        return NULL;
+
+    /* Don't decode twice */
+    if (p_descriptor->p_decoded)
+        return p_descriptor->p_decoded;
+
+    /* Allocate memory */
+    p_decoded = (dvbpsi_network_name_dr_t*)calloc(1, sizeof(dvbpsi_network_name_dr_t));
+    if (!p_decoded)
+        return NULL;
+
+    /* Decode data */
+    p_decoded->i_name_length = p_descriptor->i_length <= 255 ?
+                               p_descriptor->i_lenght : 255;
+    if (p_decoded->i_name_length)
+        memcpy(p_decoded->i_name_byte,
+               p_descriptor->p_data,
+               p_decoded->i_name_length);
+
+    p_descriptor->p_decoded = (void*)p_decoded;
+
+    return p_decoded;
+}
+
+
+/*****************************************************************************
+ * dvbpsi_GenNetworkNameDr
+ *****************************************************************************/
+dvbpsi_descriptor_t * dvbpsi_GenNetworkNameDr(
+                                        dvbpsi_network_name_dr_t * p_decoded,
+                                        bool b_duplicate)
+{
+    /* Create the descriptor */
+    dvbpsi_descriptor_t * p_descriptor =
+            dvbpsi_NewDescriptor(0x40, p_decoded->i_name_length, NULL);
+
+    if (!p_descriptor)
+        return NULL;
+
+    /* Encode data */
+    if (p_decoded->i_name_length)
+        memcpy(p_descriptor->p_data,
+               p_decoded->i_name_byte,
+               p_decoded->i_name_length);
+
+    if (b_duplicate)
+    {
+        /* Duplicate decoded data */
+        dvbpsi_network_name_dr_t * p_dup_decoded =
+                (dvbpsi_network_name_dr_t*)calloc(1, sizeof(dvbpsi_network_name_dr_t));
+        if(p_dup_decoded)
+            memcpy(p_dup_decoded, p_decoded, sizeof(dvbpsi_network_name_dr_t));
+
+        p_descriptor->p_decoded = (void*)p_dup_decoded;
+    }
+
+    return p_descriptor;
+}
