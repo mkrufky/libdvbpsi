30fad8b6f33bccb23bc74682c5e08275958ecb5a
Roberto Corno <corno.roberto@gmail.com>
2012-08-30 11:32:19 +0200

    0x4A Descriptor comment fix
    Signed-off-by: Jean-Paul Saman <jpsaman@videolan.org>

--- src/descriptors/dr_4a.c
+++ src/descriptors/dr_4a.c
@@ -2,7 +2,7 @@
  * dr_4a.c
  * Copyright (C) 2012 VideoLAN
  *
- * Authors: rcorno (May 22, 2012)
+ * Authors: Roberto Corno <corno.roberto@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- src/descriptors/dr_4a.h
+++ src/descriptors/dr_4a.h
@@ -2,7 +2,7 @@
  * dr_4a.h
  * Copyright (C) 2012 VideoLAN
  *
- * Authors: rcorno (May 22, 2012)
+ * Authors: Corno Roberto <corno.roberto@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
