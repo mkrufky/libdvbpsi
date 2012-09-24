88da6739b393a18372fbeabfe5a14578eb587ec1
Jean-Paul Saman <jean-paul.saman@m2x.nl>
2012-09-24 14:23:20 +0200

    Revert "iso_639_languague_code_t is defined in two descriptors."
    Found better solution.
    
    This reverts commit 3b68bd173175b706fac438782cc2fdcff2f4556b.

--- src/descriptors/dr.h
+++ src/descriptors/dr.h
@@ -32,13 +32,6 @@
 #ifndef _DVBPSI_DR_H_
 #define _DVBPSI_DR_H_
 
-/* common definitions */
-#ifndef DVBPSI_ISO639_LANGUAGE_CODE_T
-#define DVBPSI_ISO639_LANGUAGE_CODE_T 1
-typedef uint8_t iso_639_language_code_t[3]; /*!< ISO639 three letter language codes */
-#endif
-
-/* Descriptors */
 #include "dr_02.h"
 #include "dr_03.h"
 #include "dr_04.h"
