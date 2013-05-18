cd7549a794f87eecd0512df08a3e8d7acd1e3a8e
Michael Krufky <mkrufky@linuxtv.org>
2013-05-18 09:26:45 -0400

    add support for parsing service location descriptor 0xA1

---
+++ src/descriptors/dr_a1.h
@@ -0,0 +1,95 @@
+#ifndef _DVBPSI_DR_a1_H_
+#define _DVBPSI_DR_a1_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct dvbpsi_service_location_element_s
+{
+  uint8_t     i_stream_type;
+  uint16_t    i_elementary_PID;
+  char        i_ISO_639_language_code[3];
+
+  struct dvbpsi_service_location_element_s * p_next;
+
+} dvbpsi_service_location_element_t;
+
+/*****************************************************************************
+ * dvbpsi_service_location_dr_s
+ *****************************************************************************/
+/*!
+ * \struct dvbpsi_service_dr_s
+ * \brief "service" descriptor structure.
+ *
+ * This structure is used to store a decoded "service location"
+ * descriptor.
+ */
+/*!
+ * \typedef struct dvbpsi_service_location_dr_s dvbpsi_service_location_dr_t
+ * \brief dvbpsi_service_dr_t type definition.
+ */
+typedef struct dvbpsi_service_location_dr_s
+{
+  uint16_t     i_PCR_PID;             /*!< PID of the Transport Stream packets*/
+  uint8_t      i_number_elements;     /*!< number of elements used for this service */
+
+  dvbpsi_service_location_element_t * p_first_element;
+
+} dvbpsi_service_location_dr_t;
+
+
+/*****************************************************************************
+ * dvbpsi_DecodeServiceDataDr
+ *****************************************************************************/
+/*!
+ * \fn dvbpsi_service_dr_t * dvbpsi_DecodeServiceLocationDr(
+                                        dvbpsi_descriptor_t * p_descriptor)
+ * \brief "service location" descriptor decoder.
+ * \param p_descriptor pointer to the descriptor structure
+ * \return a pointer to a new "service" descriptor structure
+ * which contains the decoded data.
+ */
+dvbpsi_service_location_dr_t* dvbpsi_DecodeServiceLocationDr(
+                                        dvbpsi_descriptor_t * p_descriptor);
+
+
+/*****************************************************************************
+ * dvbpsi_FreeServiceLocationDr
+ *****************************************************************************/
+/*!
+ * \fn void  dvbpsi_FreeServiceLocationDr(
+                                        dvbpsi_service_location_dr_t * p_descriptor)
+ * \brief frees service location descriptor
+ * \param p_descriptor pointer to the descriptor structure
+ */
+void dvbpsi_FreeServiceLocationDr(dvbpsi_service_location_dr_t * p_descriptor);
+#if 0
+/*****************************************************************************
+ * dvbpsi_GenServiceDataDr
+ *****************************************************************************/
+/*!
+ * \fn dvbpsi_descriptor_t * dvbpsi_GenServiceDr(
+                        dvbpsi_service_dr_t * p_decoded, bool b_duplicate)
+ * \brief "service" descriptor generator.
+ * \param p_decoded pointer to a decoded "service" descriptor
+ * structure
+ * \param b_duplicate if true then duplicate the p_decoded structure into
+ * the descriptor
+ * \return a pointer to a new descriptor structure which contains encoded data.
+ */
+dvbpsi_descriptor_t * dvbpsi_GenServiceLocationDr(
+                                        dvbpsi_service_dr_t * p_decoded,
+                                        bool b_duplicate);
+#endif
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#else
+#error "Multiple inclusions of dr_a1.h"
+#endif
+
