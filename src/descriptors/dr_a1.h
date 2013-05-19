bad9abe4a5b2a332b56bd1850e0e0dae7fc2381b
Michael Krufky <mkrufky@linuxtv.org>
2013-05-19 14:57:42 -0400

    dr_a1: fix capitalization
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>

--- src/descriptors/dr_a1.h
+++ src/descriptors/dr_a1.h
@@ -21,8 +21,8 @@ Service Location Descriptor.
 
 */
 
-#ifndef _DVBPSI_DR_a1_H_
-#define _DVBPSI_DR_a1_H_
+#ifndef _DVBPSI_DR_A1_H_
+#define _DVBPSI_DR_A1_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,7 +32,7 @@ extern "C" {
 typedef struct dvbpsi_service_location_element_s
 {
   uint8_t     i_stream_type;
-  uint16_t    i_elementary_PID;
+  uint16_t    i_elementary_pid;
   char        i_iso_639_code[3];
 
   struct dvbpsi_service_location_element_s * p_next;
@@ -55,7 +55,7 @@ typedef struct dvbpsi_service_location_element_s
  */
 typedef struct dvbpsi_service_location_dr_s
 {
-  uint16_t     i_PCR_PID;             /*!< PID of the Transport Stream packets*/
+  uint16_t     i_pcr_pid;             /*!< PID of the Transport Stream packets*/
   uint8_t      i_number_elements;     /*!< number of elements used for this service */
 
   dvbpsi_service_location_element_t * p_first_element;
