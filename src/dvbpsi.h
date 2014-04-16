a2b39ffbea7aff1a9e29bd2b90c55da02cd2aba7
Michael Ira Krufky <mkrufky@linuxtv.org>
2014-04-15 21:38:15 -0400

    dvbpsi.h: update DVBPSI_VERSION & DVBPSI_VERSION_INT
    I've noticed from commit f93983a7559508e7c1d44ce3cabf7b7558b9ea4f &
    the NEWS file that libdvbpsi is on version 1.2.0 now, but in the header
    file dvbpsi.h, DVBPSI_VERSION still says 1.1.2 and DVBPSI_VERSION_INT
    still says 1.1.1 ...
    
    Signed-off-by: Michael Ira Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Jean-Paul Saman <jpsaman@videolan.org>

--- src/dvbpsi.h
+++ src/dvbpsi.h
@@ -35,8 +35,8 @@
 #ifndef _DVBPSI_DVBPSI_H_
 #define _DVBPSI_DVBPSI_H_
 
-#define DVBPSI_VERSION      1.1.2              /*!< Human readible DVBPSI version*/
-#define DVBPSI_VERSION_INT  ((1<<16)+(1<<8)+1) /*!< Machine readible DVBPSI version */
+#define DVBPSI_VERSION      1.2.0              /*!< Human readible DVBPSI version*/
+#define DVBPSI_VERSION_INT  ((1<<16)+(2<<8)+0) /*!< Machine readible DVBPSI version */
 
 #ifdef __cplusplus
 extern "C" {
