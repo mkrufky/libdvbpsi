25cdb9fd7ce804ec69b91fa1b0887435c474b32d
Jean-Baptiste Kempf <jb@videolan.org>
2014-08-12 04:04:47 +0200

    Correctly flag mpeg2 video data in video_stream_descriptor
    See ISO/IEC 13818-1 : 2000, table 2-40 and 2.6.3
    
    See https://trac.videolan.org/vlc/ticket/4200
    
    Signed-off-by: Jean-Paul Saman <jpsaman@videolan.org>

--- src/descriptors/dr_02.c
+++ src/descriptors/dr_02.c
@@ -60,7 +60,7 @@ dvbpsi_vstream_dr_t * dvbpsi_DecodeVStreamDr(dvbpsi_descriptor_t * p_descriptor)
   if(!p_decoded) return NULL;
 
   /* Decode data and check the length */
-  p_decoded->b_mpeg2 = (p_descriptor->p_data[0] & 0x04) ? true : false;
+  p_decoded->b_mpeg2 = !((p_descriptor->p_data[0] & 0x04) ? true : false);
   if(    (!p_decoded->b_mpeg2 && (p_descriptor->i_length != 1))
       || (p_decoded->b_mpeg2 && (p_descriptor->i_length != 3)))
   {
